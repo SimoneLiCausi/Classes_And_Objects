bfeb997e8acb772230e313cad56e3cdb0b195902
Simone Li Causi <simonelicausi96@gmail.com>
2023-06-19 19:37:12 +0200

    Initial commit

---
+++ src/Exercise.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Exercise {
+
+    //Create a Java class called Programmer
+    //The Programmer class will have the following instance variables:
+    //name as a string
+    //age as an integer
+    //wearsGlasses as a boolean
+    //The Programmer class avrà i seguenti metodi:
+    //drinkCoffe() - that prints in console Espresso is the secret!
+    //printDetails() - that prints in console NameHere is a AgeHere-yo programmer
+    //hasGlasses - that prints in console Is NameHere wearing glasses? followed by the value of the dedicated variable
+    //create a Java tester class called TestProgrammers where you:
+    //create 2 Programmer objects (class instance)
+    //define the state of the objects by assigning values to the instance variables
+    //call the drinkCoffee() and printDetails() methods for the first Programmer object
+    //call the printDetails() and hasGlasses() methods for the second Programmer object
+
+
+    public static void main(String[] args) {
+
+        Programmer firstProgrammerObject = new Programmer();
+        Programmer secondProgrammerObject = new Programmer();
+
+        firstProgrammerObject.drinkCoffe();
+        firstProgrammerObject.printDetails();
+
+        System.out.println("----------------------------------------");
+
+        secondProgrammerObject.printDetails();
+        secondProgrammerObject.hasGlasses();
+    }
+    }
+
---
+++ src/Programmer.java
@@ -0,0 +1,27 @@
+public class Programmer {
+
+String name = "Simone";
+Integer age = 27;
+boolean wearGlasses = false;
+
+void drinkCoffe() {
+    System.out.println("Espresso is the secret!");
+}
+
+    void printDetails() {
+        System.out.println(name + " is a " + age + "-yo programmer");
+
+    }
+
+    void hasGlasses() {
+        System.out.println("Is " + name + " wearing glasses? " + wearGlasses);
+    }
+}
+
+
+
+
+
+
+
+
